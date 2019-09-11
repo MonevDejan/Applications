31b99a0948efe4013ebd80b0005ae0caa88bbe68
Dejan Monev <monevdejan2@gmail.com>
2019-09-11 17:02:03 +0200

    Add mvc from scratch. Di, controlers, models, repository pattern

---
+++ MVC_Core/EmployeeManagement/EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+
+        }
+        public string Index()
+        {
+           return _employeeRepository.GetEmployee(1).Name;
+        }
+
+        public string Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+    }
+}
---
+++ MVC_Core/EmployeeManagement/EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Marry1", Department = "HR1", Email = "marry1@gmail.com"},
+                new Employee() { Id = 2, Name = "Marry2", Department = "HR2", Email = "marry2@gmail.com"},
+                new Employee() { Id = 3, Name = "Marry3", Department = "HR3", Email = "marry3@gmail.com"}
+            };
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
